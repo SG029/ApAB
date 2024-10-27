42c7d4a7862fabd01c79fd2729893829d50e26b3
Sushant <sushantsg29@gmail.com>
2024-10-27 19:21:20 +0530

    made the game screen

--- core/src/main/java/com/angrybird/screens/GameScreen.java
+++ core/src/main/java/com/angrybird/screens/GameScreen.java
@@ -4,17 +4,22 @@ import com.angrybird.AngryBirdGame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class GameScreen implements Screen {
     private final AngryBirdGame game;
     private final Stage stage;
-    private Skin skin;
+    private Texture backgroundTexture, pauseButtonTexture;
 
     public GameScreen(final AngryBirdGame game) {
         this.game = game;
@@ -22,12 +27,27 @@ public class GameScreen implements Screen {
 
         Gdx.input.setInputProcessor(stage);
 
-        skin = new Skin(Gdx.files.internal("uiskin.json"));
+        // Load the background texture
+        backgroundTexture = new Texture(Gdx.files.internal("game-back.png"));
+        Image backgroundImage = new Image(backgroundTexture);
+        backgroundImage.setSize(1280, 720);  // Adjust to screen size
 
-        // Create Pause button
-        TextButton pauseButton = new TextButton("Pause", skin);
-        pauseButton.setPosition(700, 550);
-        pauseButton.setSize(100, 50);
+        // Add background image to the stage first so it is drawn behind other elements
+        stage.addActor(backgroundImage);
+
+        // Load pause button texture
+        pauseButtonTexture = new Texture(Gdx.files.internal("pause.png"));
+        ImageButton pauseButton = new ImageButton(new TextureRegionDrawable(pauseButtonTexture));
+
+        // Set button size and position on the top-left corner
+
+        pauseButton.setSize(120, 120);  // Set to appropriate size based on image dimensions
+        float pauseY = 720 - pauseButton.getHeight() - 10;
+        pauseButton.setPosition(10, pauseY);  // Positioned with padding from top-left
+
+        addHoverEffect(pauseButton,pauseY);
+
+        // Add click listener to pause button
         pauseButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -35,7 +55,22 @@ public class GameScreen implements Screen {
             }
         });
 
+        // Add pause button to the stage after background
         stage.addActor(pauseButton);
+    }
+
+    private void addHoverEffect(ImageButton button, float originalY) {
+        button.addListener(new InputListener() {
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+                button.addAction(Actions.moveTo(button.getX(), originalY + 0.005f * 720, 0.1f));
+            }
+
+            @Override
+            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+                button.addAction(Actions.moveTo(button.getX(), originalY, 0.1f));
+            }
+        });
     }
 
     @Override
@@ -69,6 +104,7 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
         stage.dispose();
-        skin.dispose();
+        if (backgroundTexture != null) backgroundTexture.dispose();
+        if (pauseButtonTexture != null) pauseButtonTexture.dispose();
     }
 }
